fba60e8b31289ad0ed5fc6e8f37916eafa7c4336
Thibautguerin <thibaut.guerin@edu.devinci.fr>
2021-01-25 14:55:06 +0100

    Fix some bugs

--- CameraController/Assets/Scripts/CameraController.cs
+++ CameraController/Assets/Scripts/CameraController.cs
@@ -36,7 +36,7 @@ public class CameraController : MonoBehaviour
         }
         else
         {
-            if (useCameraConfiguration1)
+            if (lastConfiguration)
                 isMoving = cameraConfiguration1.LerpConfig(gameObject, cameraConfiguration2, lerpDuration);
             else
                 isMoving = cameraConfiguration2.LerpConfig(gameObject, cameraConfiguration1, lerpDuration);
@@ -59,40 +59,41 @@ public class CameraConfiguration
     [Range(0, 180)]
     public float fov = 60;
 
-    private float lerpTest;
+    [HideInInspector]
+    public float timeElapsed;
 
     public void UpdateConfig(GameObject camera)
     {
-        Camera test = camera.GetComponent<Camera>();
+        Camera cameraComponent = camera.GetComponent<Camera>();
         
         camera.transform.rotation = Quaternion.Euler(pitch, yaw, roll);
         camera.transform.position = pivot + camera.transform.rotation * Vector3.back * distance;
-        test.fieldOfView = fov;
+        cameraComponent.fieldOfView = fov;
     }
 
     public bool LerpConfig(GameObject camera, CameraConfiguration actualConfiguration, float lerpDuration)
     {
-        Camera test = camera.GetComponent<Camera>();
+        Camera cameraComponent = camera.GetComponent<Camera>();
 
-        if (lerpTest < lerpDuration)
+        if (timeElapsed < lerpDuration)
         {
-            float newPitch = Mathf.Lerp(actualConfiguration.pitch, pitch, lerpTest / lerpDuration);
-            float newYaw = Mathf.Lerp(actualConfiguration.yaw, yaw, lerpTest / lerpDuration);
-            float newRoll = Mathf.Lerp(actualConfiguration.roll, roll, lerpTest / lerpDuration);
-            Vector3 newPivot = Vector3.Lerp(actualConfiguration.pivot, pivot, lerpTest);
-            float newDistance = Mathf.Lerp(actualConfiguration.distance, distance, lerpTest);
-            float newFov = Mathf.Lerp(actualConfiguration.fov, fov, lerpTest / lerpDuration);
+            float newPitch = Mathf.Lerp(actualConfiguration.pitch, pitch, timeElapsed / lerpDuration);
+            float newYaw = Mathf.Lerp(actualConfiguration.yaw, yaw, timeElapsed / lerpDuration);
+            float newRoll = Mathf.Lerp(actualConfiguration.roll, roll, timeElapsed / lerpDuration);
+            Vector3 newPivot = Vector3.Lerp(actualConfiguration.pivot, pivot, timeElapsed / lerpDuration);
+            float newDistance = Mathf.Lerp(actualConfiguration.distance, distance, timeElapsed / lerpDuration);
+            float newFov = Mathf.Lerp(actualConfiguration.fov, fov, timeElapsed / lerpDuration);
 
             camera.transform.rotation = Quaternion.Euler(newPitch, newYaw, newRoll);
             camera.transform.position = newPivot + camera.transform.rotation * Vector3.back * newDistance;
-            test.fieldOfView = newFov;
+            cameraComponent.fieldOfView = newFov;
 
-            lerpTest += Time.deltaTime;
+            timeElapsed += Time.deltaTime;
             return true;
         }
         else
         {
-            lerpTest = 0;
+            timeElapsed = 0;
             return false;
         }
     }
